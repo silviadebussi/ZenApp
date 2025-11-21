4178e9634a256ccafee02b69a7f6404755ae77cd
silvia debussi <silviadebussi2@gmail.com>
2025-11-21 14:18:59 -0300

    status pra loading

--- screens/inspiracao.js
+++ screens/inspiracao.js
@@ -12,43 +12,60 @@ export default function Inspirar() {
   const theme = useColorScheme();
   const styles = getGlobalStyles(theme === "dark");
 
- async function fetchQuotes() {
-  try {
-    setLoading(true);
-    setError(false);
+  async function fetchQuotes() {
+    try {
+      setLoading(true);
+      setError(false);
 
-    const response = await fetch(
-      `https://api.adviceslip.com/advice?t=${Date.now()}`
-    );
-    const data = await response.json();
+      const response = await fetch(
+        `https://api.adviceslip.com/advice?t=${Date.now()}`
+      );
+      const data = await response.json();
 
-    setQuotes([data.slip.advice]);
-  } catch (e) {
-    setError(true);
-  } finally {
-    setLoading(false);
+      setQuotes([data.slip.advice]);
+    } catch (e) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
   }
-}
+
   useEffect(() => {
     fetchQuotes();
   }, []);
 
   return (
     <SafeAreaView style={styles.screen}>
-      {loading && <ActivityIndicator size="large" />}
 
-      {error && (
-        <Text style={styles.text}>Erro ao carregar frases 😓</Text>
+     
+      {loading && (
+        <View style={{ alignItems: "center", marginBottom: 20 }}>
+          <ActivityIndicator size="large" color="#4A8E7F" />
+          <Text style={[styles.mutedText, { marginTop: 10 }]}>
+            Carregando frase...
+          </Text>
+        </View>
       )}
 
+    
+      {error && (
+        <Text style={[styles.text, { marginBottom: 20 }]}>
+          Erro ao carregar frases 😓
+        </Text>
+      )}
+
+      
       {!loading && !error && (
         <View style={styles.card}>
           {quotes.map((q, i) => (
-            <Text key={i} style={styles.text}>“{q}”</Text>
+            <Text key={i} style={styles.text}>
+              “{q}”
+            </Text>
           ))}
         </View>
       )}
 
+   
       {!loading && (
         <TouchableOpacity style={styles.button} onPress={fetchQuotes}>
           <Text style={styles.buttonText}>Atualizar</Text>
