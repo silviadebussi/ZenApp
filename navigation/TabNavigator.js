9aac9f66299b7340f3d2710044ffc343ce63e0c9
silvia debussi <silviadebussi2@gmail.com>
2025-11-21 14:36:13 -0300

    icones na navegacao

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -2,15 +2,70 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../screens/Home";
 import Favoritos from "../screens/Favoritos";
 import Inspirar from "../screens/inspiracao";
+import { Image } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigator() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Inspirar" component={Inspirar} />
-      <Tab.Screen name="Favoritos" component={Favoritos} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: true,
+        tabBarStyle: { height: 70, paddingBottom: 10 }, // aumentei a altura e padding
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={require("../assets/casa.png")}
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? "#4CAF50" : "#888",
+              }}
+              resizeMode="contain"
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Inspirar"
+        component={Inspirar}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={require("../assets/lampada.png")}
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? "#4CAF50" : "#888",
+              }}
+              resizeMode="contain"
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Favoritos"
+        component={Favoritos}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={require("../assets/coracao.png")}
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? "#4CAF50" : "#888",
+              }}
+              resizeMode="contain"
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
